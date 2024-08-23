52605692f3a1d5ccc427d641f9d1905763e5638a
Niharika Khanna <118755402+nkhanna94@users.noreply.github.com>
2024-08-24 00:36:08 +0530

    Update 01-histograms-of-tweets.sql

--- datalemur/easy/01-histograms-of-tweets.sql
+++ datalemur/easy/01-histograms-of-tweets.sql
@@ -1,12 +1,27 @@
-SELECT 
-  tweet_count_per_user AS tweet_bucket, 
-  COUNT(user_id) AS users_num 
+-- Method 1 :- CTE 
+-- First, we're grouping the users by the number of tweets they posted in 2022
+WITH tweets_2022 AS (
+  SELECT user_id, COUNT(*) AS tweet_bucket
+  FROM tweets
+  WHERE EXTRACT(YEAR FROM tweet_date) = '2022'
+  GROUP BY user_id
+)
+
+-- Next, we're counting how many users fall into each group,
+-- basically figuring out how many users posted the same number of tweets, 
+-- like how many users posted exactly 1 tweet, 2 tweets, and so on.
+SELECT tweet_bucket, COUNT(*) AS user_num
+FROM tweets_2022
+GROUP BY tweet_bucket;
+
+
+-- Method 2 :- Subquery
+SELECT tweet_bucket, COUNT(*) AS user_num
 FROM (
-  SELECT 
-    user_id, 
-    COUNT(tweet_id) AS tweet_count_per_user 
-  FROM tweets 
-  WHERE tweet_date BETWEEN '2022-01-01' 
-    AND '2022-12-31'
-  GROUP BY user_id) AS total_tweets 
-GROUP BY tweet_count_per_user;
+    SELECT user_id, COUNT(*) AS tweet_bucket
+    FROM tweets
+    WHERE EXTRACT(YEAR FROM tweet_date) = '2022'
+    GROUP BY user_id
+) AS tweet_counts
+GROUP BY tweet_bucket;
+
